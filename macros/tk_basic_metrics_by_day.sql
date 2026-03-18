43e0f174d36a63c4cfacf72fee91ec558ac7efa7
Peter Wang <pw33537515@gmail.com>
2026-03-18 17:04:52 +1300

    safe cast to float before int

--- macros/tk_basic_metrics_by_day.sql
+++ macros/tk_basic_metrics_by_day.sql
@@ -3,8 +3,8 @@ ad_data AS (
   SELECT 
     SAFE_CAST(JSON_VALUE(data, '$.ad_id') AS INT64) AS ad_id,
     SAFE_CAST(JSON_VALUE(data,'$.spend') AS FLOAT64) AS media_cost,
-    SAFE_CAST(JSON_VALUE(data, '$.impressions') AS INT64) AS impressions,
-    SAFE_CAST(JSON_VALUE(data, '$.clicks') AS INT64) AS clicks,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.impressions') AS FLOAT64) AS INT64) AS impressions,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.clicks') AS FLOAT64) AS INT64) AS clicks,
     JSON_VALUE(data, '$.stat_time_day') AS date,
     ROW_NUMBER() OVER (PARTITION BY SAFE_CAST(JSON_VALUE(data, '$.ad_id') AS INT64),JSON_VALUE(data, '$.stat_time_day')) as row_num
   FROM {{ source(source_name, table_name) }}
--- macros/tk_video_metrics_by_day.sql
+++ macros/tk_video_metrics_by_day.sql
@@ -2,12 +2,12 @@
 ad_video AS (
   SELECT 
     SAFE_CAST(JSON_VALUE(data, '$.ad_id') AS INT64) AS ad_id,
-    SAFE_CAST(JSON_VALUE(data, '$.video_views_p25') AS INT64) AS video_25_completion,   
-    SAFE_CAST(JSON_VALUE(data, '$.video_views_p50') AS INT64) AS video_50_completion,
-    SAFE_CAST(JSON_VALUE(data, '$.video_views_p75') AS INT64) AS video_75_completion,
-    SAFE_CAST(JSON_VALUE(data, '$.video_views_p100') AS INT64) AS video_100_completion,
-    SAFE_CAST(JSON_VALUE(data, '$.video_play_actions') AS INT64) AS video_play,
-    SAFE_CAST(JSON_VALUE(data, "$.video_watched_2s") AS INT64) AS video_views,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.video_views_p25') AS FLOAT64) AS INT64) AS video_25_completion,   
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.video_views_p50') AS FLOAT64) AS INT64) AS video_50_completion,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.video_views_p75') AS FLOAT64) AS INT64) AS video_75_completion,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.video_views_p100') AS FLOAT64) AS INT64) AS video_100_completion,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, '$.video_play_actions') AS FLOAT64) AS INT64) AS video_play,
+    SAFE_CAST(SAFE_CAST(JSON_VALUE(data, "$.video_watched_2s") AS FLOAT64) AS INT64) AS video_views,
     JSON_VALUE(data, '$.stat_time_day') AS date,
     ROW_NUMBER() OVER (PARTITION BY SAFE_CAST(JSON_VALUE(data, '$.ad_id') AS INT64),JSON_VALUE(data, '$.stat_time_day')) as row_num
   FROM {{ source(source_name, table_name) }}
