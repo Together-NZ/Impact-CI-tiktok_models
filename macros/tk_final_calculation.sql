a8eb4543a54de1da4d7180d3e4f820026b4ba711
Peter Wang <pw33537515@gmail.com>
2026-03-31 15:52:51 +1300

    take last element as creative descr

--- macros/tk_final_calculation.sql
+++ macros/tk_final_calculation.sql
@@ -41,9 +41,9 @@ ad_name as creative_name,
     END AS media_format,
     CASE WHEN ARRAY_LENGTH(SPLIT(ad_name,'_'))>=8 THEN SPLIT(ad_name, '_')[OFFSET(5)] ELSE 'Other' END AS ad_format_detail,
     CASE WHEN ARRAY_LENGTH(SPLIT(ad_name,'_'))>=8 THEN SPLIT(ad_name, '_')[OFFSET(6)] ELSE 'Other' END AS ad_format,
-    CASE WHEN ARRAY_LENGTH(SPLIT(ad_name,'_'))>=8 THEN SPLIT(ad_name, '_')[OFFSET(7)] ELSE 'Other' END  AS creative_descr,
-    CASE WHEN ARRAY_LENGTH(SPLIT(campaign_name,'_'))>=2 THEN SPLIT(campaign_name,'_')[OFFSET(1)] ELSE campaign_name END AS campaign_descr
+    SPLIT(ad_name, '_')[OFFSET(ARRAY_LENGTH(SPLIT(ad_name, '_'))-1)] AS creative_descr,
+    SPLIT(campaign_name,'_')[OFFSET(1)] AS campaign_descr
 FROM 
     final
 
-{% endmacro %}
+    
