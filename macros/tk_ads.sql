9ed34041cbc839e9b8dc8502182f1c7690a1beef
Peter Wang <pw33537515@gmail.com>
2026-08-17 11:42:36 +1200

    modified fetching on newest campaign and ad group

--- macros/tk_ads.sql
+++ macros/tk_ads.sql
@@ -1,33 +1,35 @@
-{% macro tk_ads(source_name, ad_table_name, campaign_table_name) %}
+{% macro tk_ads(source_name, ad_table_name, campaign_table_name,adgroup_table_name) %}
 ad_campaign_duplicate AS (SELECT 
 trim(JSON_VALUE(data,'$.ad_id')) as ad_id,
 trim(JSON_VALUE(data,'$.ad_name')) as ad_name,
 trim(JSON_VALUE(data,'$.campaign_id')) as campaign_id,
 trim(JSON_VALUE(data,'$.adgroup_id')) as adgroup_id,
 ROW_NUMBER() OVER (PARTITION BY trim(JSON_VALUE(data,'$.ad_id')) ORDER BY trim(JSON_VALUE(data,'$.modify_time')) DESC) as row_num
-FROM {{ source(source_name, ad_table_name) }} ),
-ad_data AS (
+FROM {{source(source_name, ad_table_name)}} ),
+ad_data_basic AS (
   SELECT * FROM ad_campaign_duplicate WHERE row_num = 1
 ),
 campaign_data AS (
   SELECT TRIM(JSON_VALUE(data,'$.campaign_id')) AS campaign_id,
   TRIM(JSON_VALUE(data,'$.campaign_name')) AS campaign_name,
   _sdc_extracted_at as _sdc_extracted_at,
-  FROM {{ source(source_name, campaign_table_name) }}
+  
   ROW_NUMBER() OVER (PARTITION BY TRIM(JSON_VALUE(data,'$.campaign_id')) ORDER BY _sdc_extracted_at DESC) as row_num
+  FROM {{source(source_name, campaign_table_name)}}
 ),
 deduplicate_campaign_data AS (
   SELECT * FROM campaign_data WHERE row_num = 1
 ),
 ad_campaign_no_adgroup as (
-  SELECT ad_campaign.* except(campaign_id), deduplicate_campaign_data.* FROM ad_campaign LEFT JOIN deduplicate_campaign_data ON ad_campaign.campaign_id = deduplicate_campaign_data.campaign_id
+  SELECT ad_data_basic.* except(campaign_id), deduplicate_campaign_data.* FROM ad_data_basic LEFT JOIN deduplicate_campaign_data ON ad_data_basic.campaign_id = deduplicate_campaign_data.campaign_id
 ),
 adgroup_data AS (
   SELECT TRIM(JSON_VALUE(data,'$.adgroup_id')) AS adgroup_id,
   TRIM(JSON_VALUE(data,'$.adgroup_name')) AS adgroup_name,
   _sdc_extracted_at as _sdc_extracted_at,
-  FROM {{ source(source_name, adgroup_table_name) }}
+  
   ROW_NUMBER() OVER (PARTITION BY TRIM(JSON_VALUE(data,'$.adgroup_id')) ORDER BY _sdc_extracted_at DESC) as row_num
+  FROM {{source(source_name, adgroup_table_name)}}
 ),
 deduplicate_adgroup_data AS (
   SELECT * FROM adgroup_data WHERE row_num = 1
