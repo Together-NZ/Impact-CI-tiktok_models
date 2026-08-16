63167684198e8c2ee0b2281b51bc5d1594a0301a
Peter Wang <pw33537515@gmail.com>
2026-08-17 11:28:57 +1200

    always fetch newest ad group

--- macros/tk_ads.sql
+++ macros/tk_ads.sql
@@ -19,8 +19,21 @@ campaign_data AS (
 deduplicate_campaign_data AS (
   SELECT * FROM campaign_data WHERE row_num = 1
 ),
-ad_campaign as (
+ad_campaign_no_adgroup as (
   SELECT ad_campaign.* except(campaign_id), deduplicate_campaign_data.* FROM ad_campaign LEFT JOIN deduplicate_campaign_data ON ad_campaign.campaign_id = deduplicate_campaign_data.campaign_id
+),
+adgroup_data AS (
+  SELECT TRIM(JSON_VALUE(data,'$.adgroup_id')) AS adgroup_id,
+  TRIM(JSON_VALUE(data,'$.adgroup_name')) AS adgroup_name,
+  _sdc_extracted_at as _sdc_extracted_at,
+  FROM {{ source(source_name, adgroup_table_name) }}
+  ROW_NUMBER() OVER (PARTITION BY TRIM(JSON_VALUE(data,'$.adgroup_id')) ORDER BY _sdc_extracted_at DESC) as row_num
+),
+deduplicate_adgroup_data AS (
+  SELECT * FROM adgroup_data WHERE row_num = 1
+),
+ad_campaign AS (
+  SELECT ad_campaign_no_adgroup.* except(adgroup_id,campaign_id), deduplicate_adgroup_data.* FROM ad_campaign_no_adgroup LEFT JOIN deduplicate_adgroup_data ON ad_campaign_no_adgroup.adgroup_id = deduplicate_adgroup_data.adgroup_id
 )
 
 {% endmacro %}
